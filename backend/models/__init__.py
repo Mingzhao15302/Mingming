cf7ff6c27909573fda1425e1bf8cc9d089263f16
Mingzhao15302 <138959412+Mingzhao15302@users.noreply.github.com>
2025-10-25 20:51:55 +0800

    Delete backend directory

--- backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from backend.models.quote import Quote
-from backend.models.sales_log import SalesLog
-from backend.models.summary_snapshot import SummarySnapshot
-
-__all__ = ['Quote', 'SalesLog', 'SummarySnapshot']
